6c9a5ba4b1888b1b5a52f40637d3eeb0a68c433a
VlNikoAd <nikolaenko_vladislav@icloud.com>
2023-04-19 17:14:50 +0300

    readme and rebuilding my project

---
+++ src/main/java/com/example/diplom_cloud_service/DiplomCloudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.diplom_cloud_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomCloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomCloudServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diplom_cloud_service/DiplomCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplom_cloud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomCloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
